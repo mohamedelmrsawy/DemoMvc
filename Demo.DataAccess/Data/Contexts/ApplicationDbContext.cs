7543be86e4c29c0c96816970022d3da42a4d8a36
mohamed elmrsawy <mohamedelmrsawy@outlook.com>
2025-03-21 03:13:49 +0200

    1. Department Module

---
+++ Demo.DataAccess/Data/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Reflection;
+using Demo.DataAccess.Data.Configuration;
+
+namespace Demo.DataAccess.Data.Contexts
+{
+    class ApplicationDbContext : DbContext
+    {
+
+        public DbSet<Department> Departments { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("");
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration<Department>(new DepartmentConfiguration());
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            //modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+
+
+    }
+}
