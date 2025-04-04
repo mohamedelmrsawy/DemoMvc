f2f7f484eda563f15c9401213bcd7759b2b5cc84
mohamed elmrsawy <mohamedelmrsawy@outlook.com>
2025-04-04 11:55:26 +0200

    8. Employee Entity

---
+++ Demo.DataAccess/Data/Configuration/EmployeeConfiguration/EmployeeConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Demo.DataAccess.Data.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DataAccess.Data.Configuration.EmployeeConfiguration
+{
+    class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.Property(e => e.Name).HasColumnType("varchar(50)");
+            builder.Property(e => e.Address).HasColumnType("varchar(150)");
+            builder.Property(e => e.Salary).HasColumnType("decimal(10,2)");
+        
+        }
+    }
+}
---
+++ Demo.DataAccess/Data/Enum/Gender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DataAccess.Data.Enum
+{
+    public enum Gender
+    {
+        Female = 1 ,
+        Male = 2
+    }
+}
