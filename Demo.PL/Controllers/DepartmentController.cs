0e9a97df91885adce6ed6bb8558191ec67daf754
mohamed elmrsawy <mohamedelmrsawy@outlook.com>
2025-04-04 04:45:43 +0200

    4. Department Create Controller

--- Demo.PL/Controllers/DepartmentController.cs
+++ Demo.PL/Controllers/DepartmentController.cs
@@ -1,14 +1,56 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Demo.PesnL.Services;
+using Demo.PesnL.DataTransferObject;
 
 namespace Demo.PL.Controllers
 {
-    public class DepartmentController(IDepartmentServices _DeptServices) : Controller
+    public class DepartmentController(IDepartmentServices _DeptServices,
+        ILogger<DepartmentController> logger, IWebHostEnvironment _environment
+        ) : Controller
     {
         public IActionResult Index()
         {
             var depts = _DeptServices.GetAllDepartments();
             return View(depts);
         }
+
+        #region Creating Depart
+        
+        public IActionResult Create() => View();
+
+        [HttpPost]
+
+        public IActionResult Create(CreatedDepartmentDto dept)
+        {
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    int Result = _DeptServices.AddDepartment(dept);
+                    if (Result > 0)
+                        return RedirectToAction(nameof(Index));
+                    else
+                        ModelState.AddModelError(string.Empty, "Department can not creating");
+                }
+                catch(Exception ex)
+                {
+                    if (_environment.IsDevelopment())
+                    {
+                        ModelState.AddModelError(string.Empty, ex.Message);
+                    }
+                    else
+                    {
+                        logger.LogError(ex.Message);
+                    }
+                }
+            }
+
+            return View(dept);
+
+        }
+
+
+        #endregion
     }
 }
